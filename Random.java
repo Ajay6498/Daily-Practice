4af6232523e3269ebc866b694ee470fb23a68f0b
ajay <ajaybawakar19@gmail.com>
2023-12-27 18:14:46 +0530

    fc

--- Random.java
+++ Random.java
@@ -1,9 +1,18 @@
- 
+import java.util.*;
+
 public class Random {
     public static void main(String[] args) {
-        Random r = new Random();
 
-        System.out.println();
+        System.out.println(Math.random());
+
+        /****************************************************************** */
+
+        int max = 100, min = 50;
+        System.out.println("Generated numbers are within " + min + " to " + max);
+        System.out.println(min + (int) (Math.random() * ((max - min) + 1)));
+
+        /*********************************************************** */
 
     }
+
 }
