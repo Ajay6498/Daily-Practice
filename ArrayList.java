2ba53de32848b3adb94f6be65e29c6ab766aa7f4
ajay <ajaybawakar19@gmail.com>
2023-12-29 10:19:26 +0530

    fc

--- ArrayList.java
+++ ArrayList.java
@@ -1,40 +1,32 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class ArrayList {
-    public static void main(String[] args) {
-             List<Integer> ll= Arrays.asList(22,34,344,56,65,7);
-             System.out.println(ll);
+  public static void main(String[] args) {
+    List<Integer> ll = Arrays.asList(22, 34, 344, 56, 65, 7);
+    System.out.println(ll);
 
-             System.out.println(ll.get(5));//7
-              
-             System.out.println(ll.size());//6
+    System.out.println(ll.get(5));// 7
 
-             System.out.println(ll.isEmpty());//False
+    System.out.println(ll.size());// 6
 
-             System.out.println( ll.contains(65));//true
+    System.out.println(ll.isEmpty());// False
 
-             //sort
-              System.out.println(ll.stream().sorted().collect(Collectors.toList()));
-            //Reverse
-              System.out.println(ll.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList()));
-              
-              ll.sort(Comparator.naturalOrder());
-              System.out.println(ll);
+    System.out.println(ll.contains(65));// true
 
-             List<Integer> l= ll.subList(2, 4);
+    // sort
+    System.out.println(ll.stream().sorted().collect(Collectors.toList()));
+    // Reverse
+    System.out.println(ll.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList()));
 
-             System.out.println(l);
+    ll.sort(Comparator.naturalOrder());
+    System.out.println(ll);
 
+    List<Integer> l = ll.subList(2, 4);
 
+    System.out.println(l);
 
-              
-
-
-              
-
-    }
+  }
 }
